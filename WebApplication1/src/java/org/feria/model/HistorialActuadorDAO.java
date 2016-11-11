4f276cddf4984463c5520c2228d1a09ad9b35b59
ChaoticTale <jrcortest1@gmail.com>
2016-11-10 21:08:29 -0300

    HistorialActuadorDAO/HistorialSensorDAO

--- WebApplication1/src/java/org/feria/model/HistorialActuadorDAO.java
+++ WebApplication1/src/java/org/feria/model/HistorialActuadorDAO.java
@@ -1,38 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.feria.model;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.feria.bd.Conexion;
+import org.feria.bean.HistorialActuador;
 
 /**
  *
  * @author Alumno
  */
-public class HistorialActuadorDAO extends DAOAbstracto implements HistorialDispositivosDAOFactory<HistorialActuadorDAO> {
+public class HistorialActuadorDAO extends DAOAbstracto implements HistorialDispositivosDAOFactory<HistorialActuador> {
 
     public HistorialActuadorDAO(Conexion con) {
         super(con);
     }
 
     @Override
-    public List<HistorialActuadorDAO> getAllHistorialDispositivo() {
+    public List<HistorialActuador> getAllHistorialDispositivo() {
+        try {
+            int id_historial, fk_actuador, valor_historial = 0;
+            Date fecha_historial = null;
+            String fk_usuario = null;
+
+            ResultSet rs = con.executeQueryRS("SELECT * FROM historial_actuador");
+
+            List<HistorialActuador> lista = new ArrayList<>();
+
+            while (rs.next()) {
+                id_historial = rs.getInt(1);
+                fk_actuador = rs.getInt(2);
+                fecha_historial = rs.getDate(3);
+                valor_historial = rs.getInt(4);
+                fk_usuario = rs.getString(5);
+
+                HistorialActuador hActuador = new HistorialActuador(
+                        id_historial, fk_actuador, fecha_historial, valor_historial, fk_usuario
+                );
+
+                lista.add(hActuador);
+            }
+            return lista;
+        } catch (SQLException ex) {
+            Logger.getLogger(HistorialActuadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public List<HistorialActuador> getHistorialDispositivoBetween(Date initialDate, Date endDate) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<HistorialActuadorDAO> getHistorialDispositivoBetween(Date initialDate, Date endDate) {
+    public HistorialActuador getLastHistoricoDispositivo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public HistorialActuadorDAO getLastHistoricoDispositivo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
--- WebApplication1/src/java/org/feria/model/HistorialSensorDAO.java
+++ WebApplication1/src/java/org/feria/model/HistorialSensorDAO.java
@@ -6,33 +6,62 @@
 package org.feria.model;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.feria.bd.Conexion;
+import org.feria.bean.HistorialSensor;
 
 /**
  *
  * @author Alumno
  */
-public class HistorialSensorDAO implements HistorialDispositivosDAOFactory<HistorialSensorDAO>{
-    
-    private final Connection con;
-    
-    public HistorialSensorDAO(Connection con){
-        this.con = con;
+public class HistorialSensorDAO extends DAOAbstracto implements HistorialDispositivosDAOFactory<HistorialSensor>{
+       
+    public HistorialSensorDAO(Conexion con){
+        super(con);
     }
 
     @Override
-    public List<HistorialSensorDAO> getAllHistorialDispositivo() {
+    public List<HistorialSensor> getAllHistorialDispositivo() {
+         try {
+            int id_historial, fk_sensor, valor_historial = 0;
+            Date fecha_historial = null;
+
+            ResultSet rs = con.executeQueryRS("SELECT * FROM historial_sensor");
+
+            List<HistorialSensor> lista = new ArrayList<>();
+
+            while (rs.next()) {
+                id_historial = rs.getInt(1);
+                fk_sensor = rs.getInt(2);
+                fecha_historial = rs.getDate(3);
+                valor_historial = rs.getInt(4);
+
+                HistorialSensor hSensor = new HistorialSensor(
+                        id_historial, fk_sensor, fecha_historial, valor_historial
+                );
+
+                lista.add(hSensor);
+            }
+            return lista;
+        } catch (SQLException ex) {
+            Logger.getLogger(HistorialActuadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @Override
+    public List<HistorialSensor> getHistorialDispositivoBetween(Date initialDate, Date endDate) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List<HistorialSensorDAO> getHistorialDispositivoBetween(Date initialDate, Date endDate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public HistorialSensorDAO getLastHistoricoDispositivo() {
+    public HistorialSensor getLastHistoricoDispositivo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
